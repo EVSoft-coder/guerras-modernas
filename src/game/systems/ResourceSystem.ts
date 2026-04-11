bef7b8a1b8848bf3990dc121b9c9ceb4af57f337
Emanuel Viegas <fotoamadorblog@gmail.com>
2026-04-11 18:18:23 +0100

    feat: tie resource production to buildings

--- src/game/systems/ResourceSystem.ts
+++ src/game/systems/ResourceSystem.ts
@@ -5,7 +5,7 @@
 import { eventBus, EventPayload } from '../../core/EventBus';
 import { entityManager } from '../../core/EntityManager';
 import { GameSystem } from '../systemsRegistry';
- 
+
 export class ResourceSystem implements GameSystem {
     public init(): void {
         console.log('[SYSTEM] ResourceSystem - Economic Core ONLINE.');
@@ -15,30 +15,48 @@ export class ResourceSystem implements GameSystem {
             this.generateResources();
         });
     }
- 
+
     private generateResources(): void {
-        const entities = entityManager.getEntitiesWith(['Resource']);
- 
+        const entities = entityManager.getEntitiesWith(['Resource', 'Building']);
+
         for (const id of entities) {
             const res = entityManager.getComponent<any>(id, 'Resource');
-            if (res) {
-                res.wood += 1;
-                res.stone += 1;
-                res.iron += 1;
+            const building = entityManager.getComponent<any>(id, 'Building');
+
+            if (res && building) {
+                const amount = building.level || 1;
+
+                switch (building.type) {
+                    case 'MINE':
+                        res.iron += amount;
+                        res.stone += Math.floor(amount / 2) || 1;
+                        break;
+                    case 'SAWMILL':
+                        res.wood += amount;
+                        break;
+                    case 'HQ':
+                        res.wood += amount;
+                        res.stone += amount;
+                        res.iron += amount;
+                        break;
+                    default:
+                        // Produção base se o edifício for reconhecido mas genérico
+                        res.wood += 1;
+                        res.stone += 1;
+                        res.iron += 1;
+                        break;
+                }
             }
         }
     }
- 
+
     public preUpdate(deltaTime: number): void {}
     public update(deltaTime: number): void {}
     public postUpdate(deltaTime: number): void {}
- 
+
     public destroy(): void {
         console.log('[SYSTEM] ResourceSystem - Economic Core OFFLINE.');
     }
 }
- 
+
 export const resourceSystem = new ResourceSystem();
-鼓
-鼓
-鼓
