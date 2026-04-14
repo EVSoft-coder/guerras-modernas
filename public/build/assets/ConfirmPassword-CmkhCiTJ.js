7de7c71667d9db570d16f3821fcc136e430de0a8
Emanuel Viegas <fotoamadorblog@gmail.com>
2026-04-14 13:46:41 +0100

    fix: normalize pages/Auth/Login.tsx casing for production

---
+++ public/build/assets/ConfirmPassword-CmkhCiTJ.js
@@ -0,0 +1,54 @@
+import { m as me, j as jsxRuntimeExports, L as Le } from "./app-BGqIn98w.js";
+import { L as Label, I as InputError } from "./label-BTYgnNh2.js";
+import { B as Button } from "./app-logo-icon-CnLAvyDx.js";
+import { I as Input } from "./index-Dl0EGl2x.js";
+import { A as AuthLayout } from "./auth-layout-uQXPVFda.js";
+import { L as LoaderCircle } from "./loader-circle-BJMPezSW.js";
+function ConfirmPassword() {
+  const { data, setData, post, processing, errors, reset } = me({
+    password: ""
+  });
+  const submit = (e) => {
+    e.preventDefault();
+    post(route("password.confirm"), {
+      onFinish: () => reset("password")
+    });
+  };
+  return /* @__PURE__ */ jsxRuntimeExports.jsxs(
+    AuthLayout,
+    {
+      title: "Confirm your password",
+      description: "This is a secure area of the application. Please confirm your password before continuing.",
+      children: [
+        /* @__PURE__ */ jsxRuntimeExports.jsx(Le, { title: "Confirm password" }),
+        /* @__PURE__ */ jsxRuntimeExports.jsx("form", { onSubmit: submit, children: /* @__PURE__ */ jsxRuntimeExports.jsxs("div", { className: "space-y-6", children: [
+          /* @__PURE__ */ jsxRuntimeExports.jsxs("div", { className: "grid gap-2", children: [
+            /* @__PURE__ */ jsxRuntimeExports.jsx(Label, { htmlFor: "password", children: "Password" }),
+            /* @__PURE__ */ jsxRuntimeExports.jsx(
+              Input,
+              {
+                id: "password",
+                type: "password",
+                name: "password",
+                placeholder: "Password",
+                autoComplete: "current-password",
+                value: data.password,
+                autoFocus: true,
+                onChange: (e) => setData("password", e.target.value)
+              }
+            ),
+            /* @__PURE__ */ jsxRuntimeExports.jsx(InputError, { message: errors.password })
+          ] }),
+          /* @__PURE__ */ jsxRuntimeExports.jsx("div", { className: "flex items-center", children: /* @__PURE__ */ jsxRuntimeExports.jsxs(Button, { className: "w-full", disabled: processing, children: [
+            processing && /* @__PURE__ */ jsxRuntimeExports.jsx(LoaderCircle, { className: "h-4 w-4 animate-spin" }),
+            "Confirm password"
+          ] }) })
+        ] }) })
+      ]
+    }
+  );
+}
+export {
+  ConfirmPassword as default
+};
+//# sourceMappingURL=ConfirmPassword-CmkhCiTJ.js.map
