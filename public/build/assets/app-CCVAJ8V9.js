9d4e7d1a1a23b5e6b5ec73db562375a69aec9817
Emanuel Viegas <fotoamadorblog@gmail.com>
2026-04-18 15:16:16 +0100

    feat(ui): premium grade overhaul - tactical terrain V10 and hyper-realistic asset fleet

--- public/build/assets/app-CCVAJ8V9.js
+++ public/build/assets/app-CCVAJ8V9.js
@@ -44465,7 +44465,7 @@ if (rootElement) {
       const isDashboard = (_f = (_e2 = (_d = props == null ? void 0 : props.initialPage) == null ? void 0 : _d.component) == null ? void 0 : _e2.toLowerCase()) == null ? void 0 : _f.includes("dashboard");
       if (isAuth && isDashboard) {
         console.log("[MOTOR] Autorização detectada. Ativando ECS Engine...");
-        __vitePreload(() => import("./index-Cqenngxb.js"), true ? [] : void 0);
+        __vitePreload(() => import("./index-CsgDIfB8.js"), true ? [] : void 0);
       } else {
         const blockingElements = ["GAME_SCREEN", "MAIN_MENU", "PAUSE_SCREEN", "village-view-container", "tactical-hud", "world-map-view"];
         blockingElements.forEach((id2) => {
@@ -44501,4 +44501,4 @@ export {
   resourceSystem as r,
   stateManager as s
 };
-//# sourceMappingURL=app-DqxVeWqw.js.map
+//# sourceMappingURL=app-CCVAJ8V9.js.map
