2349737b8a97fcda20976e91f0620e883b677db0
Emanuel Viegas <fotoamadorblog@gmail.com>
2026-04-27 00:58:46 +0100

    deploy: updated compiled assets with engine fixes

---
+++ public/build/assets/text-link-CnqieuOM.js
@@ -0,0 +1,19 @@
+import { j as jsxRuntimeExports, $ as $e } from "./app-C8Y4dPfr.js";
+import { c as cn } from "./app-logo-icon-bMrDIz9n.js";
+function TextLink({ className = "", children, ...props }) {
+  return /* @__PURE__ */ jsxRuntimeExports.jsx(
+    $e,
+    {
+      className: cn(
+        "text-foreground underline decoration-neutral-300 underline-offset-4 transition-colors duration-300 ease-out hover:decoration-current! dark:decoration-neutral-500",
+        className
+      ),
+      ...props,
+      children
+    }
+  );
+}
+export {
+  TextLink as T
+};
+//# sourceMappingURL=text-link-CnqieuOM.js.map
